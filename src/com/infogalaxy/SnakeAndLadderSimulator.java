a5bc3f56865fbdeaf797d0c51c86dc1daae5504b
Sunilashokbhoi <sunilbhoi606@gmail.com>
2022-04-06 10:38:20 +0530

    MODIFIED:Check For Options And Winning Position 100

--- src/com/infogalaxy/SnakeAndLadderSimulator.java
+++ src/com/infogalaxy/SnakeAndLadderSimulator.java
@@ -3,20 +3,50 @@ package com.infogalaxy;
 import java.util.Random;
 
 public class SnakeAndLadderSimulator {
-   //UC-1-Single Play Position
-    public void playerPosition() {
-        int position = 0;
+   //Position of Player
+   int position = 0;
+   //Define Constant For Option
+    final int NO_PLAY = 1;
+    final int LADDER = 2 ;
+    final int SNAKE = 3 ;
+    Random random = new Random();
+    //Showing The Player Cerrent Position
+    public void showPosition() {
         System.out.println("Player Position :" + position);
     }
-  //UC-2-Use Random Number Get A Number Between 1 to 6
+  //-2-Rolling The Die To Position
     public void rollDie() {
-        Random random = new Random();
-        int die = (int) ((Math.random()*(6-1))+1);
-        System.out.println("Roll Die :" + die);
+
+        while(position!= 100) {
+            int dieNo = (int) ((Math.random() *( 7 - 1)) + 1);
+            System.out.println("Roll Die :" + dieNo);
+
+            //UC-3-Checking For Options to play
+             int option = (int) ((Math.random()*(4-1))+1);
+            System.out.println("Die No. :" + option);
+            switch (option) {
+                case NO_PLAY:
+                    System.out.println("NO PLAY Arrived. Stay on Same Position");
+                    break;
+                case LADDER:
+                    System.out.println("HIRRAY !!! you get Ladder");
+                    position = position + dieNo;
+                    break;
+                case SNAKE:
+                    System.out.println("OPP !!! you get Snake");
+                    position = position - dieNo ;
+                    break;
+
+            }
+        }
     }
     public static void main(String[] args) {
-        SnakeAndLadderSimulator snake = new SnakeAndLadderSimulator();
-            snake.playerPosition();
-            snake.rollDie();
+        //Creating Player1 Object
+        SnakeAndLadderSimulator playe1 = new SnakeAndLadderSimulator();
+        //Showing Player1 position
+            playe1.showPosition();
+            //Rolling The Die
+            playe1.rollDie();
+            playe1.showPosition();
     }
 }
